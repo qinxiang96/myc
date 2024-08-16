48f093c99fc571d30f737500d5c91414fa149932
qinxiang <qinxiang96@163.com>
2024-08-16 21:36:31 +0800

    test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # myc
+笔记
+define预处理器：可以通过define的方式初始化一个常量
+#define pi 3.14f
---
+++ demo06/demo06.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a = 21;
+    int b = 10;
+    int c = 0;
+    c = a % b;
+    printf("c=a %% b = %d\n",c);
+    return 0;
+}
