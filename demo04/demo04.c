c86bd16c43f8c1413ca39e5149063cacbb97d800
qinxiang <qinxiang96@163.com>
2024-08-16 21:21:04 +0800

    test

---
+++ demo04/demo04.c
@@ -0,0 +1,14 @@
+// #include <stdio.h>
+// int main(){
+//     int weight = 60;
+//     weight = 70;
+//     printf("weight = %d\n",weight);
+//     printf("weight = %d\n",weight);
+//     return 0;
+// }
+#include <stdio.h>
+int main(){
+    int intsize = sizeof(int);
+    printf("int ¥Ê¥¢¥Û–°£∫%d \n",intsize);
+    return 0;
+}
---
+++ demo05/demo05.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    unsigned char weight1,weight2;
+    weight1 = 60;
+    weight2 = 61;
+    printf("weight1 = %d kg, weight2 = %d kg\n",weight1,weight2);
+    return 0;
+}
